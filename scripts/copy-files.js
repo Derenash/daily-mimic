5f4558f260ffd2b57778854d560860e14c83f1d3
Derenash <eyer.derenash@gmail.com>
2024-07-02 12:28:50 -0300

    update gitignore

---
+++ scripts/copy-files.js
@@ -0,0 +1,28 @@
+const fs = require('fs-extra');
+const glob = require('glob');
+
+// Array of file patterns to copy
+const filesToCopy = [
+  'src/html/*',
+  'src/css/*',
+  'src/assets/*'
+];
+
+// Destination directory
+const destDir = 'dist';
+
+// Copy files based on the patterns
+filesToCopy.forEach(pattern => {
+  glob(pattern, (err, files) => {
+    if (err) {
+      console.error(`Error copying files: ${err}`);
+      return;
+    }
+
+    files.forEach(file => {
+      const destPath = file.replace('src', destDir);
+      fs.copySync(file, destPath);
+      console.log(`Copied ${file} to ${destPath}`);
+    });
+  });
+});
